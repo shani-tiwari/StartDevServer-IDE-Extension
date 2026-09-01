1c3bb2edbc4e5137baf6a1f416ef69163a69d3da
shani-tiwari <shanitiwari2021@gmail.com>
2026-09-01 19:15:55 +0530

    added badges

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,8 +1,20 @@
+import Image from "next/image";
 import Link from "next/link";
 
 export default function Footer(){
   return (
-    <footer className="w-full h-20 flex flex-col md:flex-row items-center justify-between my-6 font-sans">
+    <footer className="w-full h-28 flex flex-col gap-4 md:flex-row items-center justify-between  font-sans">
+
+        <a href="https://twelve.tools" 
+            target="_blank">
+                <Image src="https://twelve.tools/badge2-dark.svg" alt="Featured on Twelve Tools" width="160" height="40"/>
+        </a>
+        <a href="https://www.producthunt.com/products/sds-start-dev-server?embed=true&amp;utm_source=badge-featured&amp;utm_medium=badge&amp;utm_campaign=badge-sds-start-dev-server" 
+            target="_blank" 
+            rel="noopener noreferrer">
+             <Image alt="SDS - Start Dev Server - Run your React/Next project in a click with auto re-direct. | Product Hunt" width="200" height="54" src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=1238520&amp;theme=neutral&amp;t=1788269945200"/>
+        </a>
+
         {/* copy right info  */}
         <div className="flex flex-col ">
             <p className="text-sm text-foreground font-semibold">
@@ -13,7 +25,7 @@ export default function Footer(){
             </p>
         </div>
         {/* connect with me section  */}
-        <div className="flex items-center gap-4">
+        <div className="flex h-full items-center justify-center gap-4">
             <Link className="hover:translate-y-1 transition-all duration-300" href='mailto:[shanitiwari2021@gmail.com]'>
                 Email me
             </Link>
